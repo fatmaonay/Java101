64a7c83afd17637fab2b3d69055cea19d5346a47
fatmaonay <123073745+fatmaonay@users.noreply.github.com>
2023-03-12 14:05:20 +0300

    Create KDVTutar.java

---
+++ Practises/KDVTutar.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+      //Değişkenlerimizi oluşturduk.
+      double tutar, kdvOran, kdvTutar, kdvliTutar;
+      
+      //Scanner sınıfımızı tanımlıyoruz.
+      Scanner inp = new Scanner(System.in);
+      
+      //Kullanıcıdan değerleri alıyoruz.
+      System.out.print("Alınacak Tutarı Giriniz: ");
+      tutar = inp.nextDouble();
+      
+      kdvOran = (tutar>0 && tutar<1000 ? 0.18 : 0.08);
+      kdvTutar = tutar * kdvOran;
+      kdvliTutar = tutar + kdvTutar;
+     
+      System.out.println("KDV'siz Tutarı Giriniz: " + tutar);
+      System.out.println("KDV Oranı: " + kdvOran);
+      System.out.println("KDV Tutarı: " + kdvTutar);
+      System.out.println("KDV'li Tutar: " + kdvliTutar);
+      
+    }
+}
