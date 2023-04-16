a659c94de7c4224f2dbe3befc8a8b3baded209b6
fatmaonay <123073745+fatmaonay@users.noreply.github.com>
2023-04-16 14:24:30 +0300

    Create artikYil.java

---
+++ Homeworks/artikYil.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class MyClass {
+    public static void main(String args[]) {
+      int year;
+      Scanner inp = new Scanner(System.in);
+      
+      System.out.print("Yılı giriniz: ");
+      year = inp.nextInt();
+      
+      if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)){
+          System.out.println(year + " yılı artık yıldır.");
+      }else{
+          System.out.println(year + " yılı artık bir yıl değildir.");
+      }
+    }
+}
