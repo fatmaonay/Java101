6ee773a60724d9aa3d8a2fb9b39759a83db8021f
fatmaonay <123073745+fatmaonay@users.noreply.github.com>
2023-09-25 14:01:25 +0300

    Update and rename

--- Homeworks/ChineseZodiac.java
+++ Homeworks/ChineseZodiac.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MyClass {
+public class ChineseZodiac {
     public static void main(String args[]) {
       int birthYear;
       Scanner inp = new Scanner(System.in);
