cba3a0f96ca9fcc4c70affcbfd1ad26468ab97ce
fatmaonay <123073745+fatmaonay@users.noreply.github.com>
2023-09-25 14:00:43 +0300

    Update and rename

--- Homeworks/ArtikYil.java
+++ Homeworks/ArtikYil.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MyClass {
+public class ArtikYil {
     public static void main(String args[]) {
       int year;
       Scanner inp = new Scanner(System.in);
